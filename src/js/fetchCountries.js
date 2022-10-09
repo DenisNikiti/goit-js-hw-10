ee56b04663be73fd39ab7fc85f85d99754e8bc15
DenisNikiti <den.hikitin@gmail.com>
2022-10-09 21:14:57 +0300

    5

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,4 +1,5 @@
 import Notiflix from 'notiflix';
+
 function fetchCountries(e) {
           const name = e.target.value.trim()
     fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`).then(response => response.json()).then(data => {
