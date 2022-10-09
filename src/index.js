4bf3d84a2c66183a46b50d960fa138999e5156d7
DenisNikiti <den.hikitin@gmail.com>
2022-10-09 20:53:48 +0300

    3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import { fetchCountries } from "./js/fetchCountries";
-
+import Notiflix from 'notiflix';
+var debounce = require('lodash.debounce');
 
 const inputEl = document.querySelector(".input-js")
+   
+inputEl.addEventListener("input", debounce(fetchCountries,300))
+  
+function input(e) {
+      
+   
+}
+
+
+
+//   Notiflix.Notify.info("фвфыв");
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,8 @@
-function fetchCountries(name) {
-    fetch("https://restcountries.com/v3.1/name/${name}").then(response => response.json())
+
+function fetchCountries(e) {
+          const name = e.target.value.trim()
+    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`).then(response => response.json())
+    
 }
 
 export { fetchCountries}
